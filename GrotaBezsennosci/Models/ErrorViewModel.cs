315a03c6f0c3cf80d095721be1b45c76b0f65501
Gniewini <pawelgniew02@gmail.com>
2023-01-10 18:45:23 +0100

    Dodaj pliki projektów.

---
+++ GrotaBezsennosci/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace GrotaBezsennosci.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
